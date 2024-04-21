32780f4de681c518379962b032cb7e73ec914950
Gabriel Santana Neves <gabrielsantananeves@gmail.com>
2024-04-21 17:47:02 -0300

    criado pastas de controller e service para a entidade usuario

--- amorsaude-web/amorsaude-api/dist/app.module.js
+++ amorsaude-web/amorsaude-api/dist/app.module.js
@@ -8,8 +8,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
 const auth_module_1 = require("./auth/auth.module");
 const amorsaude_module_1 = require("./amorsaude/amorsaude.module");
 const usuario_module_1 = require("./usuario/controller/usuario.module");
@@ -18,9 +16,7 @@ let AppModule = class AppModule {
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [auth_module_1.AuthModule, amorsaude_module_1.AmorsaudeModule, usuario_module_1.UsuarioModule],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        imports: [auth_module_1.AuthModule, amorsaude_module_1.AmorsaudeModule, usuario_module_1.UsuarioModule]
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
--- amorsaude-web/amorsaude-api/src/app.module.ts
+++ amorsaude-web/amorsaude-api/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AmorsaudeModule } from './amorsaude/amorsaude.module';
 import { UsuarioModule } from './usuario/controller/usuario.module';
 
 @Module({
-  imports: [AuthModule, AmorsaudeModule, UsuarioModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, AmorsaudeModule, UsuarioModule]
 })
 export class AppModule {}
