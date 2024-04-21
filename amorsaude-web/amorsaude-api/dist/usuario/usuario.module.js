8c86507a9592eb11479793565f70accec01e828d
Gabriel Santana Neves <gabrielsantananeves@gmail.com>
2024-04-21 17:28:30 -0300

    add resource de usuario

--- amorsaude-web/amorsaude-api/dist/usuario/usuario.module.js
+++ amorsaude-web/amorsaude-api/dist/usuario/usuario.module.js
@@ -6,21 +6,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
+exports.UsuarioModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
-const auth_module_1 = require("./auth/auth.module");
-const amorsaude_module_1 = require("./amorsaude/amorsaude.module");
-const usuario_module_1 = require("./usuario/usuario.module");
-let AppModule = class AppModule {
+const usuario_service_1 = require("./usuario.service");
+const usuario_controller_1 = require("./usuario.controller");
+let UsuarioModule = class UsuarioModule {
 };
-exports.AppModule = AppModule;
-exports.AppModule = AppModule = __decorate([
+exports.UsuarioModule = UsuarioModule;
+exports.UsuarioModule = UsuarioModule = __decorate([
     (0, common_1.Module)({
-        imports: [auth_module_1.AuthModule, amorsaude_module_1.AmorsaudeModule, usuario_module_1.UsuarioModule],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        controllers: [usuario_controller_1.UsuarioController],
+        providers: [usuario_service_1.UsuarioService],
     })
-], AppModule);
-//# sourceMappingURL=app.module.js.map
+], UsuarioModule);
+//# sourceMappingURL=usuario.module.js.map
--- amorsaude-web/amorsaude-api/src/app.module.ts
+++ amorsaude-web/amorsaude-api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AmorsaudeModule } from './amorsaude/amorsaude.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AmorsaudeModule, UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
