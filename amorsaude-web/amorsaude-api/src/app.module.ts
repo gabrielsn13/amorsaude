8495c7a550507621e0224acfbd154866f88cf725
Gabriel Santana Neves <gabrielsantananeves@gmail.com>
2024-04-21 18:31:30 -0300

    add nestjs config no projeto

--- amorsaude-web/amorsaude-api/src/app.module.ts
+++ amorsaude-web/amorsaude-api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AmorsaudeModule } from './amorsaude/amorsaude.module';
 import { UsuarioModule } from './usuario/controller/usuario.module';
 
 @Module({
-  imports: [AuthModule, AmorsaudeModule, UsuarioModule]
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    AuthModule, 
+    AmorsaudeModule, 
+    UsuarioModule]
 })
 export class AppModule {}
