fec1ddf2a76e59c8e0a8eb7ddebee92b8048d945
Gabriel Santana Neves <gabrielsantananeves@gmail.com>
2024-04-22 13:34:38 -0300

    ajustes no arquivo do docker

--- amorsaude-web/amorsaude-api/src/usuario/entities/usuario.entity.ts
+++ amorsaude-web/amorsaude-api/src/usuario/entities/usuario.entity.ts
@@ -1,1 +1,22 @@
-export class Usuario {}
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
+import { AmorsaudeModule } from 'src/amorsaude/amorsaude.module';
+import { UsuarioModule } from '../controller/usuario.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        autoLoadEntities: true,
+        synchronize: true
+    }),
+    AuthModule, 
+    AmorsaudeModule, 
+    UsuarioModule
+]
+})
+export class AppModule {}
