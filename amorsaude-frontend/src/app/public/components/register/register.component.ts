6ceff19ec0d114b6f12674d7ceaa10d28919309d
gabriel.neves <gabriel.neves@comlink.com.br>
2024-04-24 14:30:28 -0300

    adicionado formulario de login

--- amorsaude-frontend/src/app/public/components/register/register.component.ts
+++ amorsaude-frontend/src/app/public/components/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from '../../services/usuario-service/usuario.service';
+import { Router } from '@angular/router';
+import {tap} from "rxjs";
+import { CustomValidators } from '../../validators/custom-validators';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,42 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required]),
+    username: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+    passwordConfirm: new FormControl(null, [Validators.required])
+  },{validators: CustomValidators.passwordsMatching}
+);
 
+constructor(private userService: UsuarioService, private router: Router) { }
+
+register() {
+  if(this.form.valid) {
+    this.userService.register({
+      email: this.email.value,
+      username: this.username.value,
+      password: this.password.value
+    }).pipe(
+      tap(() => this.router.navigate(['../login']))
+    )
+      .subscribe();
+  }
+}
+
+get email(): FormControl {
+  return this.form.get('email') as FormControl;
+}
+
+get password(): FormControl {
+  return this.form.get('password') as FormControl;
+}
+
+get passwordConfirm(): FormControl {
+  return this.form.get('passwordConfirm') as FormControl;
+}
+
+get username(): FormControl {
+  return this.form.get('username') as FormControl;
+}
 }
