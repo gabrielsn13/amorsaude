b5370d4c77533058a6db9b4700afaeab98645d1c
gabriel.neves <gabriel.neves@comlink.com.br>
2024-04-24 14:47:30 -0300

    adicionado formulário de login para o projeto

--- amorsaude-frontend/src/app/public/components/login/login.component.ts
+++ amorsaude-frontend/src/app/public/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from '../../services/usuario-service/usuario.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required])
+  });
 
+  constructor(private userService: UsuarioService, private router: Router) { }
+
+  login() {
+    if (this.form.valid) {
+      this.userService.login({
+        email: this.email.value,
+        password: this.password.value
+      }).pipe(
+        tap(() => this.router.navigate(['../../private/dashboard']))
+      ).subscribe();
+    }
+  }
+
+  get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
 }
