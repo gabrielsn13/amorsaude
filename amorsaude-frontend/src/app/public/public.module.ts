39f427b5c3822135a44501a8d381f35f75663452
gabriel.neves <gabriel.neves@comlink.com.br>
2024-04-24 14:18:34 -0300

    ajuste na tela de login

--- amorsaude-frontend/src/app/public/public.module.ts
+++ amorsaude-frontend/src/app/public/public.module.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { PublicRoutingModule } from './public-routing.module';
-
-
+import {MatCardModule} from "@angular/material/card";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButton, MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -13,7 +16,12 @@ import { PublicRoutingModule } from './public-routing.module';
   ],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class PublicModule { }
