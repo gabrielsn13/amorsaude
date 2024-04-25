5abb3abc37e9df5e73c0319839980e561ab58f90
gabriel.neves <gabriel.neves@comlink.com.br>
2024-04-25 13:23:01 -0300

    add enable cors

--- amorsaude-frontend/src/app/public/services/usuario-service/usuario.service.ts
+++ amorsaude-frontend/src/app/public/services/usuario-service/usuario.service.ts
@@ -21,7 +21,7 @@ export class UsuarioService {
   ) { }
 
   login(usuario: IUsuario): Observable<ILoginResponse>{
-    return this.httpClient.post<ILoginResponse>('http://localhost:3000/api/usuario/login', usuario).pipe(
+    return this.httpClient.post<  >('http://localhost:3000/api/usuario/login', usuario).pipe(
       tap((res: ILoginResponse) => localStorage.setItem('amorsaude_app', res.access_token)),
       tap(() => this.snackbar.open('Sucesso Login!', 'Close', snackBarConfig)),
       catchError(e => {
@@ -32,6 +32,7 @@ export class UsuarioService {
   }
 
   register(usuario: IUsuario): Observable<IUsuario>{
+    console.log('teste', usuario);
     return this.httpClient.post<IUsuario>('http://localhost:3000/api/usuario', usuario).pipe(
       tap((usuarioCriado: IUsuario) => this.snackbar.open(`Usuario ${usuarioCriado.username} foi criado com sucesso!`, 'Close', snackBarConfig)),
       catchError(e => {
