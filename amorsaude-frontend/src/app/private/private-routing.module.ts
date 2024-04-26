00492f0bd0c8d300797b2c10442187b87708d448
gabriel.neves <gabriel.neves@comlink.com.br>
2024-04-26 13:04:03 -0300

    removido componente menu e lista

--- amorsaude-frontend/src/app/private/private-routing.module.ts
+++ amorsaude-frontend/src/app/private/private-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ListaUsuariosComponent } from "./components/lista-usuarios/lista-usuarios.component";
 import { MenuComponent } from "./components/menu/menu.component";
 
 export const routes: Routes = [
     {
         path: 'menu',
         component: MenuComponent
-    },
-    {
-        path: 'lista-usuarios',
-        component: ListaUsuariosComponent
     },
     {
         path: '**',
