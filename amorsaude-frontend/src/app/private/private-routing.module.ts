5966e7f78bcabd521c471efd419ba47e7e4a38ad
gabriel.neves <gabriel.neves@comlink.com.br>
2024-04-26 16:15:02 -0300

    add menu mat toolbar

--- amorsaude-frontend/src/app/private/private-routing.module.ts
+++ amorsaude-frontend/src/app/private/private-routing.module.ts
@@ -15,7 +15,8 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class PrivateRoutingModule {
 
--- amorsaude-frontend/src/app/private/private.module.ts
+++ amorsaude-frontend/src/app/private/private.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrivateRoutingModule } from './private-routing.module';
 import { MenuComponent } from './components/menu/menu.component';
-
+import { MatToolbarModule} from "@angular/material/toolbar";
 
 
 @NgModule({
 //declarations: [MenuComponent],
   imports: [
     CommonModule,
-    PrivateRoutingModule
+    PrivateRoutingModule,
+    MatToolbarModule
   ],
 declarations: [
   MenuComponent
